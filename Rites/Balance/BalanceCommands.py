02f2e8f35e15b27fa0f41d13c6358de7e3635881
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-07-16 15:40:19 +0200

    BalanceCommands introduced.

---
+++ Rites/Balance/BalanceCommands.py
@@ -0,0 +1,41 @@
+"""Balance rite commands."""
+
+import Rites.RiteCommon
+
+class BalanceCommandCommandWorkExecution(object):
+    """CommandWorkExecution command.
+
+    Attributes:
+        mName:    The name of the command.
+        mMessage: The CommandWorkExecution.
+
+    """
+
+    def __init__(self, aMessage):
+        """Initializes the command.
+
+        Arguments:
+            aMessage: The PresentYourselfRequest.
+
+        """
+        self.mName    = 'CommandWorkExecution'
+        self.mMessage = aMessage
+
+    def execute(self, aCommandProcessor):
+        """Executes the command.
+
+        Arguments:
+            aCommandProcessor: The command processor to be visited.
+
+        """
+        # Get the list of known critters.
+        knownCritters = aCommandProcessor.mRites[Rites.RiteCommon.REGISTRY].getKnownCritters()
+
+        # Filter workers.
+        # TODO.
+
+        # Balance the load.
+        # TODO.
+
+        # Command work execution.
+        # TODO.
