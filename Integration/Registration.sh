4f22c4e1096c023fd34651d4a16839491188cb77
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-08-16 05:42:59 +0200

    A very dummy facilitator.

---
+++ Integration/Registration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BROADCAST_DAEMON="/home/brian/workspace/Critter/BroadcastDaemon.py"
+SUT="/home/brian/workspace/Critter/HelloCrittyMain.py HelloCritty1"
+TEST="/home/brian/workspace/Critter/Integration/Registration.py"
+
+python $BROADCAST_DAEMON &
+BROADCAST_DAEMON_PID=$!
+
+python $SUT &
+SUT_PID=$!
+
+python $TEST
+echo $?
+
+kill -9 $SUT_PID
+kill -9 $BROADCAST_DAEMON_PID
