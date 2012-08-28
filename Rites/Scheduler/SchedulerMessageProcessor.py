275df76e82aabbbea3a937f0fcaf969c899da62b
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-08-28 18:49:27 +0200

    Comments removed. A small fix introduced.

--- Rites/Scheduler/SchedulerMessageProcessor.py
+++ Rites/Scheduler/SchedulerMessageProcessor.py
@@ -1,5 +1,3 @@
-"""The message processor of scheduler rite."""
-
 import Rites.RiteCommon
 
 from Rites.MessageProcessor            import MessageProcessor
@@ -8,29 +6,15 @@ from Rites.Scheduler.SchedulerCommands import SchedulerCommand_Handle_ExecuteGra
 from Rites.Scheduler.SchedulerCommands import SchedulerCommand_Handle_ExecuteGraphVoluntee
 
 class SchedulerMessageProcessor(MessageProcessor):
-    """The message processor of the scheduler rite."""
-
     def __init__(self, aRite):
-        """Initializes the message processor.
-
-        Arguments:
-            aRite: The rite.
-
-        """
         MessageProcessor.__init__(self, aRite)
 
     def processMessage(self, aMessage):
-        """Processes the message.
-
-        Arguments:
-            aMessage: The message.
-
-        """
         command = None
 
-        if aMessage.messageName == 'ExecuteGraphSeekVolunteers':  command = SchedulerCommand_Handle_ExecuteGraphSeekVolunteers(aMessage)
-        if aMessage.messageName == 'ExecuteGraphSelectVolunteer': command = SchedulerCommand_Handle_ExecuteGraphSelectVolunteer(aMessage)
-        if aMessage.messageName == 'ExecuteGraphVoluntee':        command = SchedulerCommand_Handle_ExecuteGraphVoluntee(aMessage)
+        if   aMessage.messageName == 'ExecuteGraphSeekVolunteers':  command = SchedulerCommand_Handle_ExecuteGraphSeekVolunteers(aMessage)
+        elif aMessage.messageName == 'ExecuteGraphSelectVolunteer': command = SchedulerCommand_Handle_ExecuteGraphSelectVolunteer(aMessage)
+        elif aMessage.messageName == 'ExecuteGraphVoluntee':        command = SchedulerCommand_Handle_ExecuteGraphVoluntee(aMessage)
 
         if command:
             self.mRite.mPostOffice.putCommand(Rites.RiteCommon.SCHEDULER, command)
