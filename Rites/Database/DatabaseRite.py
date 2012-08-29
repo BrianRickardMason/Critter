4a28189b262c37d2c04bfd80c6ba625ac40f246c
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-08-29 18:02:33 +0200

    Obsolete Captain Obvious comments removed.

--- Rites/Database/DatabaseRite.py
+++ Rites/Database/DatabaseRite.py
@@ -1,5 +1,3 @@
-"""The database rite."""
-
 import time
 
 import Rites.RiteCommon
@@ -8,24 +6,7 @@ from Rites.Database.DatabaseMessageProcessor import DatabaseMessageProcessor
 from Rites.Rite                              import Rite
 
 class DatabaseRite(Rite):
-    """The database rite.
-
-    Attributes:
-        mCommandProcessor: The command processor of the rite
-        mMessageProcessor: The message processor of the rite
-
-    """
-
     def __init__(self, aCritter, aCritterData, aSettings, aPostOffice):
-        """Initializes the rite.
-
-        Arguments:
-            aCritter:     The critter.
-            aCritterData: The critter data.
-            aSettings:    The settings.
-            aPostOffice:  The post office.
-
-        """
         Rite.__init__(self,
                       aCritter,
                       aCritterData,
@@ -35,7 +16,6 @@ class DatabaseRite(Rite):
                       DatabaseMessageProcessor)
 
     def run(self):
-        """Starts the main loop of the rite."""
         while True:
             self.mLogger.debug("Sleeping for a heartbeat.")
             time.sleep(self.mSettings.get('heartbeat', 'period'))
