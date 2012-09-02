a7896e6e0f64679d4d67d72ffcaa213124153aad
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-09-02 13:39:54 +0200

    Captain Obvious comments removed. Obsolete imports removed.

--- Rites/Heartbeat/HeartbeatMessageProcessor.py
+++ Rites/Heartbeat/HeartbeatMessageProcessor.py
@@ -1,26 +1,8 @@
-"""The message processor of heartbeat rite."""
-
-import Rites.RiteCommon
-
 from Rites.MessageProcessor import MessageProcessor
 
 class HeartbeatMessageProcessor(MessageProcessor):
-    """The message processor of the heartbeat rite."""
-
     def __init__(self, aRite):
-        """Initializes the message processor.
-
-        Arguments:
-            aRite: The rite.
-
-        """
         MessageProcessor.__init__(self, aRite)
 
     def processMessage(self, aMessage):
-        """Processes the message.
-
-        Arguments:
-            aMessage: The message.
-
-        """
         self.mLogger.debug("Dropping unknown message: %s" % aMessage.messageName)
--- Rites/Poke/PokeMessageProcessor.py
+++ Rites/Poke/PokeMessageProcessor.py
@@ -1,26 +1,8 @@
-"""The message processor of poke rite."""
-
-import Rites.RiteCommon
-
 from Rites.MessageProcessor import MessageProcessor
 
 class PokeMessageProcessor(MessageProcessor):
-    """The message processor of the poke rite."""
-
     def __init__(self, aRite):
-        """Initializes the message processor.
-
-        Arguments:
-            aRite: The rite.
-
-        """
         MessageProcessor.__init__(self, aRite)
 
     def processMessage(self, aMessage):
-        """Processes the message.
-
-        Arguments:
-            aMessage: The message.
-
-        """
         self.mLogger.debug("Dropping unknown message: %s" % aMessage.messageName)
