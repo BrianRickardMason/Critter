e21e9f010ce7bb6748c421d78de1c60b47e48452
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-09-02 17:14:23 +0200

    A cosmetic change.

--- Rites/Balance/BalanceMessageProcessor.py
+++ Rites/Balance/BalanceMessageProcessor.py
@@ -13,9 +13,9 @@ class BalanceMessageProcessor(MessageProcessor):
         command = None
 
         if False: pass
-        elif aMessage.messageName == 'Command_Election_Res':                command = BalanceCommand_Handle_Command_Election_Res(aMessage)
-        elif aMessage.messageName == 'Command_ExecuteWork_Res':             command = BalanceCommand_Handle_Command_ExecuteWork_Res(aMessage)
-        elif aMessage.messageName == 'Command_OrderWorkExecution_Req':      command = BalanceCommand_Handle_Command_OrderWorkExecution_Req(aMessage)
+        elif aMessage.messageName == 'Command_Election_Res':           command = BalanceCommand_Handle_Command_Election_Res(aMessage)
+        elif aMessage.messageName == 'Command_ExecuteWork_Res':        command = BalanceCommand_Handle_Command_ExecuteWork_Res(aMessage)
+        elif aMessage.messageName == 'Command_OrderWorkExecution_Req': command = BalanceCommand_Handle_Command_OrderWorkExecution_Req(aMessage)
 
         if command:
             self.mRite.mPostOffice.putCommand(Rites.RiteCommon.BALANCE, command)
