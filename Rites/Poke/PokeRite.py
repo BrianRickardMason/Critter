d112d3c36c0947b5de999244f28e1cbcd9d8d2d3
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-09-09 14:44:14 +0200

    Announcement_Poke replaces PokeAnnouncement.

--- Rites/Poke/PokeRite.py
+++ Rites/Poke/PokeRite.py
@@ -1,5 +1,3 @@
-"""The poke rite."""
-
 import time
 
 import Rites.RiteCommon
@@ -8,35 +6,18 @@ from Rites.Poke.PokeMessageProcessor import PokeMessageProcessor
 from Rites.Rite                      import Rite
 
 class PokeRite(Rite):
-    """The poke rite.
-
-    Attributes:
-        mCommandProcessor: The command processor of the rite
-        mMessageProcessor: The message processor of the rite
-
-    """
-
     def __init__(self, aCritter, aCritterData, aSettings, aPostOffice):
-        """Initializes the rite.
-
-        Arguments:
-            aCritter:     The critter.
-            aCritterData: The critter data.
-            aSettings:    The settings.
-            aPostOffice:  The post office.
-
-        """
         Rite.__init__(self, aCritter, aCritterData, aSettings, aPostOffice, Rites.RiteCommon.POKE, PokeMessageProcessor)
 
     def run(self):
-        """Starts the main loop of the rite."""
         while True:
             self.mLogger.debug("Poking other critters.")
+            messageName = 'Announcement_Poke'
             envelope = self.mPostOffice.encode(
-                'PokeAnnouncement',
-                {'messageName': 'PokeAnnouncement',
-                 'sender':      {'type': self.mCritterData.mType,
-                                 'nick': self.mCritterData.mNick}})
+                messageName,
+                {'messageName': messageName,
+                 'crittnick':   self.mCritter.mCritterData.mNick}
+            )
             self.mPostOffice.putOutgoingAnnouncement(envelope)
 
             self.mLogger.debug("Sleeping for a heartbeat.")
