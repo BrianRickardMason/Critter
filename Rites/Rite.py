7a0d82e06dafadea545e7b90024b59a442a9ef79
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-09-10 18:46:20 +0200

    Captain Obvious comments removed.

--- Rites/Rite.py
+++ Rites/Rite.py
@@ -1,5 +1,3 @@
-"""The rite."""
-
 import logging
 import threading
 
@@ -8,28 +6,7 @@ from Rites.CommandProcessor import CommandProcessor
 logging.basicConfig(format='[%(asctime)s][%(threadName)28s][%(levelname)8s] - %(message)s')
 
 class Rite(threading.Thread):
-    """The graph rite.
-
-    Attributes:
-        mCritter:    The critter.
-        mRiteName:   The name of the rite.
-        mSettings:   The settings.
-        mPostOffice: The post office.
-        mLogger:     The logger.
-
-    """
-
     def __init__(self, aCritter, aSettings, aPostOffice, aRiteName, aMessageProcessorType):
-        """Initializes the rite.
-
-        Arguments:
-            aCritter:              The critter.
-            aSettings:             The settings.
-            aPostOffice:           The post office.
-            aRiteName:             The name of the rite.
-            aMessageProcessorType: The type of the command processor.
-
-        """
         self.mSettings = aSettings
 
         self.mLogger = logging.getLogger(aRiteName + 'Rite')
@@ -52,25 +29,12 @@ class Rite(threading.Thread):
         threading.Thread.__init__(self, name=aRiteName + 'Rite')
 
     def run(self):
-        """Starts the main loop of the rite."""
         raise NotImplementedError
 
     def putCommand(self, aCommand):
-        """Puts a command into the queue of command processor.
-
-        Arguments:
-            aCommand The command.
-
-        """
         self.mCommandProcessor.put(aCommand)
 
     def putMessage(self, aMessage):
-        """Puts a message into the queue of message processor.
-
-        Arguments:
-            aMessage The message.
-
-        """
         self.mMessageProcessor.put(aMessage)
 
     def insertRecvRequest(self, aMessageName, aCritthash, aMessage, aSoftTimeout=3, aHardTimeout=5):
