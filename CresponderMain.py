c7085742ae791b0dfcd95f09d8015e7a4288c4bd
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-09-17 15:05:58 +0100

    Cresponder responses to requests.

--- CresponderMain.py
+++ CresponderMain.py
@@ -1,10 +1,31 @@
 import sys
+import threading
+import zmq
 
 from Critter.Critter import Critter
+
+class CresponderWork(threading.Thread):
+    def __init__(self):
+        self.mContext = zmq.Context()
+
+        self.mSocket = self.mContext.socket(zmq.REP)
+        self.mSocket.bind('tcp://127.0.0.1:5555')
+
+        threading.Thread.__init__(self, name='CresponderWork')
+
+    def run(self):
+        while True:
+            request = self.mSocket.recv()
+            print "Received request: '%s'" % request
+            self.mSocket.send(request)
 
 class Cresponder(Critter):
     def __init__(self, aCrittnick, aRites):
         Critter.__init__(self, aCrittnick , aCrittnick, aRites)
+
+        self.mCresponderWork = CresponderWork()
+        self.mCresponderWork.setDaemon(True)
+        self.mCresponderWork.start()
 
 if __name__ == "__main__":
     critter = Cresponder(sys.argv[1], [])
