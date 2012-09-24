525b46ac6d228d214863d0355017cc814e8fd816
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-09-24 18:32:57 +0200

    HelloCrittyMain removed.

--- HelloCrittyMain.py
+++
@@ -1,9 +0,0 @@
-"""The HelloCritty main."""
-
-import sys
-
-from Critter.Critter import Critter
-
-if __name__ == "__main__":
-    critter = Critter('HelloCritty', sys.argv[1], ['Poke'])
-    critter.run()
