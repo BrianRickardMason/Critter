97d9e0c783e0b30bf8e2510c045e35a428e1f9d4
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-09-26 18:40:30 +0100

    Captain Obvious' comments removed.

--- Settings.py
+++ Settings.py
@@ -1,21 +1,11 @@
-"""The settings read from the manifest."""
-
 import logging
 
 class Settings(object):
-    """Holds the settings read from the manifest
-
-    Attributes:
-        mSettings The dictionary of settings.
-
-    """
-
     # TODO: Error handling, e.g. missing file, invalid file, etc.
     # TODO: Pass the path to the file as a parameter. If not provided then try to read the default file.
     # TODO: Should be somehow smarter.
 
     def __init__(self, aBrokerPublisher, aBrokerSubscriber):
-        """Initializes the settings."""
         # An empty dictionary for the beginning.
         self.mSettings = {}
 
@@ -42,15 +32,6 @@ class Settings(object):
         self.mSettings['logging']['level'] = logging.INFO
 
     def get(self, aSection, aEntry):
-        """Gets a specific entry of a specific section.
-
-        Arguments:
-            aSection The name of the section.
-            aEntry   The name of the entry.
-
-        Returns The value of the entry as a string, None if not found.
-
-        """
         if aSection in self.mSettings:
             if aEntry in self.mSettings[aSection]:
                 return self.mSettings[aSection][aEntry]
