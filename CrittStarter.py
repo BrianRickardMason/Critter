f31c4f4168b5578bc283b161d929f54e1bf57713
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-09-28 13:13:51 +0100

    CrittStarter slightly redesigned.

--- CrittStarter.py
+++ CrittStarter.py
@@ -11,10 +11,15 @@ class Crittstarter(object):
     def __init__(self):
         self.mTree = ElementTree.parse('CrittWorkPlan.xml')
         self.mRoot = self.mTree.getroot()
+
         self.mSettings = {}
+        self.mCrittBrokerData = {}
+        self.mCritterData = {}
 
     def start(self):
         self.__getSettings()
+        self.__readCrittBrokers()
+        self.__readCritters()
         self.__startCrittBrokers()
         self.__startCritters()
 
@@ -27,7 +32,7 @@ class Crittstarter(object):
 
         self.mCrittBrokerData = {}
 
-    def __startCrittBrokers(self):
+    def __readCrittBrokers(self):
         crittBrokers = self.mRoot.find('infrastructure').find('crittBrokers')
 
         for crittBroker in crittBrokers:
@@ -40,6 +45,7 @@ class Crittstarter(object):
             self.mCrittBrokerData[name]['connections']['publish'] = {}
             self.mCrittBrokerData[name]['connections']['subscribe'] = {}
             self.mCrittBrokerData[name]['connections']['ui'] = {}
+            self.mCrittBrokerData[name]['connections']['brokers'] = {}
             connections = crittBroker.find('connections')
             self.mCrittBrokerData[name]['connections']['publish']['protocol'] = connections.find('publish').find('protocol').text
             self.mCrittBrokerData[name]['connections']['publish']['port'] = connections.find('publish').find('port').text
@@ -48,36 +54,51 @@ class Crittstarter(object):
             self.mCrittBrokerData[name]['connections']['ui']['protocol'] = connections.find('ui').find('protocol').text
             self.mCrittBrokerData[name]['connections']['ui']['port'] = connections.find('ui').find('port').text
 
-            self.__startCrittBroker(name)
+            for brokerConnection in connections.find('brokers'):
+                neighbourName = brokerConnection.find('name').text
+                self.mCrittBrokerData[name]['connections']['brokers'][neighbourName] = {}
+                self.mCrittBrokerData[name]['connections']['brokers'][neighbourName]['publish'] = {}
+                self.mCrittBrokerData[name]['connections']['brokers'][neighbourName]['subscribe'] = {}
+                self.mCrittBrokerData[name]['connections']['brokers'][neighbourName]['publish']['protocol'] = brokerConnection.find('publish').find('protocol').text
+                self.mCrittBrokerData[name]['connections']['brokers'][neighbourName]['publish']['port'] = brokerConnection.find('publish').find('port').text
+                self.mCrittBrokerData[name]['connections']['brokers'][neighbourName]['subscribe']['protocol'] = brokerConnection.find('subscribe').find('protocol').text
+                self.mCrittBrokerData[name]['connections']['brokers'][neighbourName]['subscribe']['port'] = brokerConnection.find('subscribe').find('port').text
 
-    def __startCritters(self):
+    def __readCritters(self):
         critters = self.mRoot.find('infrastructure').find('critters')
 
         for critter in critters:
-            critterData = {}
-
-            critterData['crittnick'] = critter.find('crittnick').text
-            critterData['host'] = critter.find('host').text
-            critterData['crittBroker'] = critter.find('crittBroker').text
-            name = critterData['crittBroker']
-            if name in self.mCrittBrokerData:
-                critterData['connections'] = {}
-                critterData['connections']['publish'] = {}
-                critterData['connections']['subscribe'] = {}
-                critterData['connections']['publish']['protocol'] = self.mCrittBrokerData[name]['connections']['subscribe']['protocol']
-                critterData['connections']['publish']['host'] = self.mCrittBrokerData[name]['host']
-                critterData['connections']['publish']['port'] = self.mCrittBrokerData[name]['connections']['subscribe']['port']
-                critterData['connections']['subscribe']['protocol'] = self.mCrittBrokerData[name]['connections']['publish']['protocol']
-                critterData['connections']['subscribe']['host'] = self.mCrittBrokerData[name]['host']
-                critterData['connections']['subscribe']['port'] = self.mCrittBrokerData[name]['connections']['publish']['port']
+            crittnick = critter.find('crittnick').text
+            self.mCritterData[crittnick] = {}
+            self.mCritterData[crittnick]['crittnick'] = critter.find('crittnick').text
+            self.mCritterData[crittnick]['host'] = critter.find('host').text
+            self.mCritterData[crittnick]['crittBroker'] = critter.find('crittBroker').text
+            # TODO: Naming convention: brokerName?
+            brokerName = self.mCritterData[crittnick]['crittBroker']
+            if brokerName in self.mCrittBrokerData:
+                self.mCritterData[crittnick]['connections'] = {}
+                self.mCritterData[crittnick]['connections']['publish'] = {}
+                self.mCritterData[crittnick]['connections']['subscribe'] = {}
+                self.mCritterData[crittnick]['connections']['publish']['protocol'] = self.mCrittBrokerData[brokerName]['connections']['subscribe']['protocol']
+                self.mCritterData[crittnick]['connections']['publish']['host'] = self.mCrittBrokerData[brokerName]['host']
+                self.mCritterData[crittnick]['connections']['publish']['port'] = self.mCrittBrokerData[brokerName]['connections']['subscribe']['port']
+                self.mCritterData[crittnick]['connections']['subscribe']['protocol'] = self.mCrittBrokerData[brokerName]['connections']['publish']['protocol']
+                self.mCritterData[crittnick]['connections']['subscribe']['host'] = self.mCrittBrokerData[brokerName]['host']
+                self.mCritterData[crittnick]['connections']['subscribe']['port'] = self.mCrittBrokerData[brokerName]['connections']['publish']['port']
             else:
                 # TODO: Make meaningful.
                 raise Exception
-            critterData['services'] = []
+            self.mCritterData[crittnick]['services'] = []
             for service in critter.find('services'):
-                critterData['services'].append(service.text)
+                self.mCritterData[crittnick]['services'].append(service.text)
 
-            self.__startCritter(critterData)
+    def __startCrittBrokers(self):
+        # TODO: Implement me!
+        pass
+
+    def __startCritters(self):
+        # TODO: Implement me!
+        pass
 
     def __startCrittBroker(self, aName):
         Popen([
